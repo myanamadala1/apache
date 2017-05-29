9b11cdefd07559c7c0e62fa114cf9ef16f123183
yanamadala1 <yanamadalamohan10@gmail.com>
2017-05-28 21:49:01 -0400

    added a template and shorterned the code in server.rb version 0.2.1 created index.html.erb

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,12 +6,9 @@
 
 package 'httpd'
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, World!</h1>
-  HOSTNAME: #{node['hostname']}
-  IPADDRESS: #{node['ipaddress']} 
-"
-end
+template 'var/www/html/index.html' do
+ source 'index.html.erb'
+end 
 
 service 'httpd' do
   action [:enable, :start]
